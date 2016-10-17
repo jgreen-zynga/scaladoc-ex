3e1a1059914db38bf71cb0380f41a866627aa886
Jason HiDef Green <jgreen@zynga.com>
2016-10-17 03:14:46 -0400

    Bunch of test types

---
+++ foo-app/src/main/scala/com/foo/bar/package.scala
@@ -0,0 +1,23 @@
+package com
+package foo
+
+package object bar {
+	def baz : java.lang.Object = ???
+
+	def wunderbar : String = ""
+
+	/**
+	 * World
+	 */ 
+	class Hello {
+
+	}
+
+	object Hello {
+		object Hi
+	}
+}
+
+object Bazzinga {
+	class Ehhhhhh
+}
